1992e50da9ef8cf480e30215bce7a3365461a239
Jack Foltz <jack@foltz.io>
2023-10-04 20:38:28 -0400

    stuff

--- examples/lights/pipeline/logic/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod beat_indicator;
-
-pub mod div_select;
-pub mod active_toggle;
-pub mod palette_switcher;
-pub mod pause_enable;
-pub mod color_select;
--- examples/lights/pipeline/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod logic;
-pub mod light;
-pub mod time;
-
-pub mod filter;
-pub mod map;
